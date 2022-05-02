b40185b9a8a3e6faa1a868585af51285c2e9a6e6
Rauchendraser <andreasr@ppedv.de>
2022-05-02 11:14:24 +0200

    VM Installation

---
+++ SQL Administration/SQL Administration/01_Virtualisierung.sql
@@ -0,0 +1,45 @@
+--HARDWARE
+--HDD RAM
+--CPU
+
+--DB (trenne Datendatei Logfile)
+
+
+--Unser Host:
+
+/*
+
+16 GB
+1 Socket 
+4 Kerne/2 Kerne
+1 HDD
+
+16 GB - (OS + Virtualisierung) -4 GB = 12 GB
+12-2048 
+
+CPU                     1
+RAM                    WINDOWS                                     SQL (MAX RAM)      CPU
+HV-DC				2048MB dynamisch																1
+HV-SQL1            statisch  5 GB									 3000MB					       4
+HV-SQL2			statisch 5 GB									 3000MB                       4
+
+
+
+schulung\Administrator
+ppedv2019!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
